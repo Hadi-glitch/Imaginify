6655cc7208ad95415a3a3cdcca33134c30284291
Hadi <abdulhadi74737@gmail.com>
2024-09-07 19:51:54 +0500

    Initial commit

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -7,14 +7,22 @@ let cached = (global as any).mongoose || { conn: null, promise: null };
 export const connectToDatabase = async () => {
   if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URL) throw new Error('MONGODB_URL is missing');
+  if (!MONGODB_URL) {
+    console.error('MONGODB_URL is missing');
+    throw new Error('MONGODB_URL is missing');
+  }
 
-  cached.promise = cached.promise || mongoose.connect(MONGODB_URL, {
-    dbName: 'imaginify',
-    bufferCommands: false,
-  })
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URL, {
+      dbName: 'imaginify',
+      bufferCommands: false,
+    }).catch(err => {
+      console.error('Error connecting to MongoDB:', err);
+      throw err;
+    });
+  }
 
   cached.conn = await cached.promise;
 
   return cached.conn;
-}
+};
